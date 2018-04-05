f2f90b63a72af330983bb0f61f696553cd9d44f2
edgar <edgrr.trem000@gmail.com>
2018-04-04 21:08:12 -0500

    add subscription for real time messaging

--- src/containers/MessageContainer.js
+++ src/containers/MessageContainer.js
@@ -5,28 +5,62 @@ import { Comment } from 'semantic-ui-react';
 
 import Messages from '../components/Messages';
 
+const newChannelMessageSubscription = gql`
+  subscription($channelId: Int!) {
+    newChannelMessage(channelId: $channelId){
+      id
+      text
+      user {
+        username
+      }
+      created_at
+    }
+  }
+`;
 
-const MessageContainer = ({ data: { loading, messages } }) =>
-  (loading ? null :
-  <Messages>
-    <Comment.Group>
-      {messages.map(m => (
-        <Comment key={`${m.id}-message`}>
-          <Comment.Content>
-            <Comment.Author as='a'>{m.user.username}t</Comment.Author>
-            <Comment.Metadata>
-              <div>{m.created_at}</div>
-            </Comment.Metadata>
-            <Comment.Text>{m.text}</Comment.Text>
-            <Comment.Actions>
-              <Comment.Action>Reply</Comment.Action>
-            </Comment.Actions>
-          </Comment.Content>
-        </Comment>
-      ))}
-    </Comment.Group>
-  </Messages>
-  );
+class MessageContainer extends React.Component {
+  componentDidMount() {
+    this.props.data.subscribeToMore({
+      document: newChannelMessageSubscription,
+      variables: {
+        channelId: this.props.channelId,
+      },
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev;
+
+        const newMessage = subscriptionData.data.newChannelMessage;
+
+        return Object.assign({}, prev, {
+          messages: [...prev.messages, newMessage],
+        });
+      },
+    });
+  }
+
+  render() {
+    const { data: { loading, messages } } = this.props;
+    return (loading ? null :
+    <Messages>
+      <Comment.Group>
+        {messages.map(m => (
+          <Comment key={`${m.id}-message`}>
+            <Comment.Content>
+              <Comment.Author as="a">{m.user.username}t</Comment.Author>
+              <Comment.Metadata>
+                <div>{m.created_at}</div>
+              </Comment.Metadata>
+              <Comment.Text>{m.text}</Comment.Text>
+              <Comment.Actions>
+                <Comment.Action>Reply</Comment.Action>
+              </Comment.Actions>
+            </Comment.Content>
+          </Comment>
+        ))}
+      </Comment.Group>
+    </Messages>
+    );
+  }
+}
 
 const messagesQuery = gql`
   query($channelId: Int!) {
